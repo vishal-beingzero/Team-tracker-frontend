97b943b87badd74912a19e372ad8967891732e69
Vishal Kumar <139634737+vishal-beingzero@users.noreply.github.com>
2024-07-02 19:23:13 +0530

    FOLLOW-UP CHANGE FOR DEOTENV

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <>
     {/* <h3>({import.meta.env})</h3> */}
-    <h4 className="text-center mt-5 mb-5">{JSON.stringify(import.meta.env.VITE_REACT_BACKEND_URI)}</h4>
       <h1 className=" text-center">This is a home page...</h1>
     </>
   );
