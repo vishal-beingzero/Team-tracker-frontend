7e6578bd1ba9bea2bff009c25630e69e386238e5
Vishal Kumar <139634737+vishal-beingzero@users.noreply.github.com>
2025-06-10 21:34:07 +0530

    updated

--- src/components/UserProblemMap.tsx
+++ src/components/UserProblemMap.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { BsArrowUp, BsArrowDown, BsArrowDownUp } from "react-icons/bs"; // Import sorting icons
-import { Tooltip } from 'react-tooltip';
 import { Loader } from './Loader';
 
 interface UserProblemStatusProps {}
